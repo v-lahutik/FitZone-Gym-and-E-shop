b69a3d6c5448a752842db2520648ad3ac18549e3
Imre <decodeimre@gmail.com>
2024-09-16 15:40:07 +0200

    header base done

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,20 +1,24 @@
 export default function Header() {
   return (
-    <header className="grid grid-cols-12 min-h-[100px]">
-      <div>Logo</div>
-      <div>
-        <nav className="flex items-center">
-          <ul>
-            <li>MEMBERSHIP</li>
-            <li>COURSES</li>
-            <li>SHOP</li>
-            <li>CONTACT US</li>
+    <header className="bg-gray-800 flex items-center p-4 sm:p-8 justify-between min-h-[50px] my-auto">
+      <div className="basis-1/4">Logo</div>
+
+      <div className="sm:hidden">BurgerIcon</div>
+      <div className="basis-1/2 flex justify-evenly">
+        <nav>
+          <ul className="hidden sm:flex space-x-8">
+            <li className="text-gray-500 hover:text-gray-400">MEMBERSHIP</li>
+            <li className="text-gray-500 hover:text-gray-400">COURSES</li>
+            <li className="text-gray-500 hover:text-gray-400">SHOP</li>
+            <li className="text-gray-500 hover:text-gray-400">CONTACT</li>
           </ul>
         </nav>
       </div>
-      <div>
-        <button>LOGIN</button>
-        <div>Cart</div>
+      <div className="basis-1/4 flex justify-end">
+        <div>
+          <button>LOGIN</button>
+        </div>
+        <div className="ml-4">Cart</div>
       </div>
     </header>
   );
