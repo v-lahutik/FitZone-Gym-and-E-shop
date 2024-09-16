f789c2dec7fc1c7f136183c81ee499284190488c
v-lahutik <v.lahutik@gmail.com>
2024-09-16 09:55:10 +0200

    user model, controller, router and validations added

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -1,14 +1,50 @@
+import { nextTick } from "process";
 import User from "../models/user.model";
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 
-export const registerUser = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { firstName, lastName, email, password } = req.body;
-        const user = new User({ firstName, lastName, email, password });
-        await user.save();
-        res.status(201).json({ msg: 'User created successfully' });
+        const { firstName, lastName, email, password, address } = req.body;
+        if (!firstName || !lastName || !email || !password) {
+            return res.status(400).json({ msg: 'Please fill all fields' });
+        }
+        const userExist= await User.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({ msg: 'User already exist' });
+        }
+       const newUser =  new User({ firstName, lastName, email, password, address });
+        await newUser.save();
+        res.status(201).json({
+            msg: 'User registration successful',
+            user: {
+                id: newUser._id,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                email: newUser.email,
+                address: newUser.address,
+            }});
     } catch (error: any) {
-        res.status(500).json({ msg: error.message });
+        next(error);
+    }
+}
+
+export const login= async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ msg: 'Please fill all fields' });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+        const isMatch = await user.comparePass(password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Email or Password is incorrect' });
+        }
+        res.status(200).json({ msg: 'User login successful' });
+    } catch (error: any) {
+       next(error);
     }
 }
--- Backend/src/models/user.model.ts
+++ Backend/src/models/user.model.ts
@@ -1,19 +1,106 @@
-import { Schema, model } from 'mongoose';
+import bcrypt from "bcrypt";
+import { Schema, model, Document } from "mongoose";
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    });
+export enum UserRole {
+  member = "member",
+  admin = "admin",
+  guest = "guest",
+}
 
-const User = model('User', userSchema);
+export interface AddressInput {
+  streetNumber: number;
+  streetName: string;
+  city: string;
+  country: string;
+  postCode: string;
+}
+const addressSchema = new Schema({
+  streetNumber: {
+      type: Number,
+      required: true,
+  },
+  streetName: {
+      type: String,
+      required: true,
+  },
+  city: {
+      type: String,
+      required: true,
+  },
+  postCode: {
+      type: String,
+      required: true
+  },
+  country: {
+      type: String,
+      required: true,
+  }
+}, { _id: false })
+
+
+export interface UserInput {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  address?: AddressInput
+}
+
+export interface UserDocument extends UserInput, Document {
+  role: string;
+  pass_changed_at?: Date; //optional field
+  is_activated: boolean;
+  comparePass(plainPassword: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<UserDocument>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+ 
+  address: {
+    type: addressSchema,
+  },
+  role: {
+    type: String,
+    enum: Object.values(UserRole),
+    default: UserRole.member,
+  },
+  
+},{timestamps: true});
+
+userSchema.pre<UserDocument>("save", async function (next) {
+    try {
+        if (!this.isModified("password")) {
+            return next();
+        }
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        this.pass_changed_at = new Date()
+       
+        next();
+    } catch (error: Error | any) {
+        next(error);
+    }
+});
+
+userSchema.methods.comparePass = async function (this: UserDocument, plainPassword: string) {
+  return await bcrypt.compare(plainPassword, this.password);
+};
+
+const User = model <UserDocument>("User", userSchema);
 export default User;
