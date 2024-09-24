d307c906567d1d688bfab0821b7ab5face9398d4
Taishi <taishi.shibamoto@dci-student.org>
2024-09-24 14:26:41 +0200

    fixed the User Schema

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -1,72 +1,89 @@
 import User from "../models/user.model";
 import Verify from "../models/verify.model";
-import { NextFunction, Request, Response } from 'express';
-import { createToken} from "../utils/helper";
+import { NextFunction, Request, Response } from "express";
+import { createToken } from "../utils/helper";
 import { createJwtToken } from "../utils/jwt";
 
+export const verifyAccount = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { verifyToken: token, uid: userId } = req.params;
 
-export const verifyAccount = async(req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { verifyToken: token, uid: userId } = req.params;
-     
-      const verified = await Verify.findOne({ token, userId });
-      if (!verified) {
-        return res.status(401).json({message: 'Verification link is not valid or has expired.'});
-        
-      }
-  
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({message: 'User not found or already deleted.'}); 
-      }
-  
-      if (user.is_activated) {
-        return res.status(400).json({message: 'User account is already activated.'});
-      }
-  
-      user.is_activated = true;
-      await user.save();
-  
-       res.status(200).json({
-      status: 'Account verified!',
-      message: 'Account has been successfully verified.',
+    const verified = await Verify.findOne({ token, userId });
+    if (!verified) {
+      return res
+        .status(401)
+        .json({ message: "Verification link is not valid or has expired." });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found or already deleted." });
+    }
+
+    if (user.is_activated) {
+      return res
+        .status(400)
+        .json({ message: "User account is already activated." });
+    }
+
+    user.is_activated = true;
+    await user.save();
+
+    res.status(200).json({
+      status: "Account verified!",
+      message: "Account has been successfully verified.",
     });
-    } catch (error) {
-      next(error);
+  } catch (error) {
+    next(error);
+  }
+};
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email,password)
+    if (!email || !password) {
+      return res.status(400).json({ msg: "Please fill all fields" });
     }
-  };
-  export const login= async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res.status(400).json({ msg: 'Please fill all fields' });
-        }
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ msg: 'Invalid credentials' });
-        }
-          if (!user.is_activated) {
-      return res.status(403).json({ error: "Please verify your email before logging in." });
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid credentials" });
     }
-        const isMatch = await user.comparePass(password);
-        if (!isMatch) {
-            return res.status(400).json({ msg: 'Email or Password is incorrect' });
-        }
-
-        const token = await createJwtToken({ id: user.id, email: user.email, role:user.role },process.env.JWT_SECRET as string)
-
-        res.cookie('token', token, {
-          expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-          httpOnly: true
-        });
-
-        res.status(200).json({ msg: 'User login successful' });
-    } catch (error: any) {
-       next(error);
+    if (!user.is_activated) {
+      return res
+        .status(403)
+        .json({ error: "Please verify your email before logging in." });
     }
-}
+    const isMatch = await user.comparePass(password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Email or Password is incorrect" });
+    }
 
+    const token = await createJwtToken(
+      { id: user.id, email: user.email, role: user.role },
+      process.env.JWT_SECRET as string
+    );
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    });
+
+    res.status(200).json({ msg: "User login successful" });
+  } catch (error: any) {
+    next(error);
+  }
+};
 
 export const logout = async (req: Request, res: Response) => {
-    res.clearCookie('token').json({ msg: 'User logged out' });
-}
+  res.clearCookie("token").json({ msg: "User logged out" });
+};
--- Backend/src/routers/courseTemplate.router.ts
+++ Backend/src/routers/courseTemplate.router.ts
@@ -5,9 +5,9 @@ import { authenticateAndCheckRoles } from "../middlewares/authAndRoles";
 const courseTemplateRouter =Router();
 
 
-courseTemplateRouter.get('/',authenticateAndCheckRoles('admin'), seeAllCourseTemplate)
-courseTemplateRouter.post('/create',authenticateAndCheckRoles('admin'),createNewCourseTemplate)
-courseTemplateRouter.delete('/delete/:tid',authenticateAndCheckRoles('admin'),deleteCourseTemplate)
-courseTemplateRouter.patch('/edit/:tid',authenticateAndCheckRoles('admin'),editCourseTemplate)
+courseTemplateRouter.get('/',authenticateAndCheckRoles('Admin'), seeAllCourseTemplate)
+courseTemplateRouter.post('/create',authenticateAndCheckRoles('Admin'),createNewCourseTemplate)
+courseTemplateRouter.delete('/delete/:tid',authenticateAndCheckRoles('Admin'),deleteCourseTemplate)
+courseTemplateRouter.patch('/edit/:tid',authenticateAndCheckRoles('Admin'),editCourseTemplate)
 
 export default courseTemplateRouter;
