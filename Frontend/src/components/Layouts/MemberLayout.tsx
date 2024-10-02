a1cb51282ecd938f395ea2a7d4b2d2917461e929
v-lahutik <v.lahutik@gmail.com>
2024-10-02 08:29:38 +0200

    profilePage component and change password

--- Frontend/src/components/Layouts/MemberLayout.tsx
+++ Frontend/src/components/Layouts/MemberLayout.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+
+import { Link, Outlet } from 'react-router-dom';
 
 export default function MemberLayout() {
-  return <div className="bg-white">MemberLayout</div>;
+  return (
+    <div className="bg-white">
+      {/* Navigation for admin sub-pages */}
+      <div className="header">MEMBER HEADER</div>
+      <div className="sidebar">
+        SIDEBAR
+        <nav>
+          <ul>
+            <li>
+              <Link to="/member/profile">Profile Page</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      {/* Renders the nested routes */}
+      <Outlet />
+    </div>
+  )
 }
