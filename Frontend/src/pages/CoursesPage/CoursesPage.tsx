d217d016fd2d8e8f963d8a675b678d3c9bc93bb1
Imre <decodeimre@gmail.com>
2024-10-11 11:45:01 +0200

    image update

--- Frontend/src/pages/CoursesPage/CoursesPage.tsx
+++ Frontend/src/pages/CoursesPage/CoursesPage.tsx
@@ -4,7 +4,7 @@ import { Link, useLocation } from 'react-router-dom';
 import Login from '../../components/Auth/Login';
 import { FaArrowRight } from 'react-icons/fa6';
 import './CoursePage.css';
-import HeroImage from '../../assets/images/Hero/hero_1_1.jpg';
+import HeroImage2 from '/src/assets/images/Hero/hero_1_1.jpg';
 import axios from 'axios';
 import { URL } from '../../utils/URL';
 import { CourseTemplate } from '../../custom.Types/courseTemplatesType';
@@ -65,8 +65,7 @@ export default function CoursesPage() {
         id="breadcrumb-section"
         className="hero bg-cover bg-center flex items-center h-[30vh]"
         style={{
-          backgroundImage:
-            `url${HeroImage}`,
+          backgroundImage: `url${HeroImage2}`
         }}
       >
         <div className="container mx-auto max-w-[1280px] px-4 ">
