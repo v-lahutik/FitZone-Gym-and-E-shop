b41c3b3f35903104c17597e128a3fc0e54f65472
Taishi <taishi.shibamoto@dci-student.org>
2024-10-17 10:08:36 +0200

    pagination works with filter properly

--- Frontend/src/components/Admin/Products/AdminProductsDisplay.tsx
+++ Frontend/src/components/Admin/Products/AdminProductsDisplay.tsx
@@ -66,6 +66,7 @@ export const AdminProductsDisplay: React.FC = () => {
       filtered = filtered.filter((product) =>
         product.productName.toLowerCase().includes(searchTerm.toLowerCase())
       );
+      setCurrentPage(1);
     }
 
     // Filter by product status
@@ -73,6 +74,7 @@ export const AdminProductsDisplay: React.FC = () => {
       filtered = filtered.filter(
         (product) => product.category.categoryName === filterCategory
       );
+      setCurrentPage(1);
     }
 
     // Filter by price (ascending)
@@ -86,7 +88,7 @@ export const AdminProductsDisplay: React.FC = () => {
     }
 
     setFilteredProducts(filtered);
-    setItemPerPage(8)
+    setItemPerPage(8);
   }, [searchTerm, filterCategory, filterByPrice, allProducts]);
 
   // handle filter
@@ -114,6 +116,29 @@ export const AdminProductsDisplay: React.FC = () => {
   //pagination
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
   const totalPages = Math.ceil(filteredProducts.length / itemsPerPage);
+
+  // stock appearance
+  const getStockBadge = (stock: number) => {
+    let badgeText: string = 'Unavailable';
+    let badgeStyle: string = 'bg-red-800 text-white';
+
+    if (stock !== undefined) {
+      if (stock > 10) {
+        badgeText = 'In Stock';
+        badgeStyle = 'bg-green-500 text-white';
+      } else if (stock > 5) {
+        badgeText = 'Low Stock';
+        badgeStyle = 'bg-yellow-500 text-white';
+      } else if (stock > 0) {
+        badgeText = 'Few Left';
+        badgeStyle = 'bg-orange-600 text-white';
+      } else {
+        badgeText = 'Out of Stock';
+        badgeStyle = 'bg-primary text-white'; // Replace 'bg-primary' with your desired color class for out of stock
+      }
+    }
+    return { badgeText, badgeStyle };
+  };
 
   return (
     <div className="p-4 min-h-screen">
@@ -153,91 +178,94 @@ export const AdminProductsDisplay: React.FC = () => {
         </select>
         <button
           onClick={() => openForm(null)}
-          className="bg-primary text-white px-4 py-2 rounded"
+          className="bg-primary text-white px-4 py-2 rounded hover:bg-red-700 transition transform duration-300"
         >
           Add New Product
         </button>
       </div>
       <section className="w-fit mx-auto flex flex-wrap md:grid-cols-2 justify-items-center justify-around gap-3 mt-1 mb-5">
         {/* Product cards */}
-        {displayedItems?.map((product) => (
-          <div
-            key={product._id}
-            className="my-3 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md"
-          >
-            <div className="mx-3 mt-3 flex h-60 overflow-hidden rounded-xl items-center justify-center">
-              <img
-                className="object-cover"
-                src={product.image}
-                alt={product.productName}
-              />
-            </div>
-            <div className="mt-4 px-5 pb-5">
-              <div className="flex justify-end">
-                <span
-                  className={`rounded-full px-2 py-1 text-center text-sm font-medium text-white ${
-                    product.stock > 0 ? 'bg-black' : 'bg-primary'
-                  }`}
-                >
-                  {product.stock > 0 ? 'In Stock' : 'Out of Stock'}
-                </span>
+        {displayedItems?.map((product) => {
+          const { badgeText, badgeStyle } = getStockBadge(product.stock);
+          return (
+            <div
+              key={product._id}
+              className="my-3 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md"
+            >
+              <div className="mx-3 mt-3 flex h-60 overflow-hidden rounded-xl items-center justify-center">
+                <img
+                  className="object-cover"
+                  src={product.image}
+                  alt={product.productName}
+                />
               </div>
-              <h5 className="text-xl tracking-tight text-slate-900">
-                {product.productName}
-              </h5>
-              <p className="text-gray-600 line-clamp-2 hover:line-clamp-4 h-12">
-                {product.description}
-              </p>
-
-              <div className="mt-2 mb-5 flex items-center justify-between">
-                <p>
-                  <span className="text-xl font-bold text-slate-900">
-                    €{product.price}
-                  </span>
-                  {/* Optional: Add a line-through for original price if available for sale*/}
-                  {/* <span className="text-sm text-slate-900 line-through">${originalPrice}</span> */}
-                </p>
-                <div className="flex items-center">
-                  {/* Stars for rating */}
-                  {[...Array(Math.round(product.averageRating))].map((_, index) => (
-                    <svg
-                      key={index}
-                      aria-hidden="true"
-                      className="h-5 w-5 text-primary"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                  ))}
-                  <span className="mr-2 ml-3 rounded bg-primary px-2.5 py-0.5 text-white text-xs font-semibold">
-                    {product.averageRating}
+              <div className="mt-4 px-5 pb-5">
+                <div className="flex justify-end">
+                  <span
+                    className={`rounded-full px-2 py-1 text-center text-sm font-medium ${badgeStyle}`}
+                  >
+                    {badgeText}
                   </span>
                 </div>
-              </div>
-              <div className="flex gap-1">
-                <button
-                  onClick={() => openForm(product)}
-                  className="w-1/2 flex items-center justify-center rounded-md bg-blackColor3 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary focus:outline-none focus:ring-4 focus:ring-blue-300"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() =>
-                    handleDeleteProduct(product, setProductChanged)
-                  }
-                  className="w-1/2 flex items-center justify-center rounded-md bg-blackColor3 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary focus:outline-none focus:ring-4 focus:ring-blue-300"
-                >
-                  {/* <ProductForm product={product} closeForm={function (): void {
+                <h5 className="text-xl tracking-tight text-slate-900">
+                  {product.productName}
+                </h5>
+                <p className="text-gray-600 line-clamp-2 hover:line-clamp-4 h-12">
+                  {product.description}
+                </p>
+
+                <div className="mt-2 mb-5 flex items-center justify-between">
+                  <p>
+                    <span className="text-xl font-bold text-slate-900">
+                      €{product.price}
+                    </span>
+                    {/* Optional: Add a line-through for original price if available for sale*/}
+                    {/* <span className="text-sm text-slate-900 line-through">${originalPrice}</span> */}
+                  </p>
+                  <div className="flex items-center">
+                    {/* Stars for rating */}
+                    {[...Array(Math.round(product.averageRating))].map(
+                      (_, index) => (
+                        <svg
+                          key={index}
+                          aria-hidden="true"
+                          className="h-5 w-5 text-primary"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                        </svg>
+                      )
+                    )}
+                    <span className="mr-2 ml-3 rounded bg-primary px-2.5 py-0.5 text-white text-xs font-semibold">
+                      {product.averageRating}
+                    </span>
+                  </div>
+                </div>
+                <div className="flex gap-1">
+                  <button
+                    onClick={() => openForm(product)}
+                    className="w-1/2 flex items-center justify-center rounded-md bg-blackColor3 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary transition transform duration-300 focus:outline-none focus:ring-4 focus:ring-blue-300"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() =>
+                      handleDeleteProduct(product, setProductChanged)
+                    }
+                    className="w-1/2 flex items-center justify-center rounded-md bg-blackColor3 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary transition transform duration-300 focus:outline-none focus:ring-4 focus:ring-blue-300"
+                  >
+                    {/* <ProductForm product={product} closeForm={function (): void {
                   throw new Error("Function not implemented.");
                 } } isEditing={false} /> */}
-                  Delete
-                </button>
+                    Delete
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
 
         {isFormOpen && (
           <ProductForm
@@ -254,7 +282,9 @@ export const AdminProductsDisplay: React.FC = () => {
           onClick={() => paginate(currentPage - 1)}
           disabled={currentPage === 1}
           className={`px-4 py-2 border border-blackColor3 rounded text-blackColor3 ${
-            currentPage === 1 ? 'cursor-not-allowed opacity-50' : 'hover:text-white hover:bg-blackColor3 transition transform duration-300 ease-in-out'
+            currentPage === 1
+              ? 'cursor-not-allowed opacity-50'
+              : 'hover:text-white hover:bg-blackColor3 transition transform duration-300 ease-in-out'
           }`}
         >
           Previous
